bc2c7b5c35372a8667cba36e44354d392eaaea1d
majianyang <majianyang@kuaishou.com>
2023-03-02 20:15:46 +0800

    feat: cypress完成

--- cypress-demo-1/cypress/e2e/demo.cy.js
+++ cypress-demo-1/cypress/e2e/demo.cy.js
@@ -4,6 +4,6 @@ describe('百度', () => {
     cy.get('input#kw').eq(0).type('饥人谷')
     cy.contains('百度一下').click()
     cy.contains('饥人谷官网').should('exist')
-    cy.contains('jirengu').should('exist')
+    // cy.contains('jirengu').should('exist')
   })
 })
